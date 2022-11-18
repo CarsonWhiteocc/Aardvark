ba9181a5f4edcb5466e40b525c5238d2b2419102
CarsonWhiteocc <98561349+CarsonWhiteocc@users.noreply.github.com>
2022-11-18 03:05:20 -0500

    Add files via upload
    Early version of our project

---
+++ AardvarkDB/src/aardvarkdb/Backend.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aardvarkdb;
+
+import java.sql.*;
+
+public class Backend 
+{
+    private Connection con = null;
+    
+    public Backend()
+    {
+        try
+        {
+            con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/?user=root", "root", 
+                ""/*Whatever password you use for your sql server*/);
+            Statement s = con.createStatement();
+            s.executeQuery("use phase1");
+        }
+        catch(Exception e)
+        {
+            System.out.println("Could not establish connection");
+        }
+    }
+    public ResultSet handleQuery(String query)
+    {
+        try
+        {
+            PreparedStatement p = con.prepareStatement(query);
+            return p.executeQuery();
+        }
+        catch(Exception e)
+        {
+            System.out.println("Could not handle query");
+            return null;
+        }
+    }
+}
