eb850d1915e5715403a273f855cb95373c68f3d2
CarsonWhiteocc <98561349+CarsonWhiteocc@users.noreply.github.com>
2022-11-21 02:49:23 -0500

    Update current_set_up.sql

--- current_set_up.sql
+++ current_set_up.sql
@@ -1,6 +1,6 @@
-create database phase1;
+create database test;
 
-use phase1;
+use test;
 
 create table BLOODTYPE (id_Bloodtype int, bType varchar(3) not null, 
 primary key (id_Bloodtype));
@@ -11,26 +11,26 @@ primary key (id_Bank));
 create table VAMPIRE(id_Vampire int, FName varchar(45) not null, 
 LName varchar(45) not null, primary key (id_Vampire));
 
-create table DONOR(id_Donor int, id_Bloodtype int not null, Lst_Donation Date, 
+create table DONOR(id_Donor int, Bloodtype_id int not null, Lst_Donation Date, 
 Pers_Info varchar(45) not null, Date_Start Date not null, primary key (id_Donor), 
-foreign key (id_Bloodtype) references BLOODTYPE(id_Bloodtype));
+foreign key (Bloodtype_id) references BLOODTYPE(id_Bloodtype));
 
 create table BLOODBAG(id_BloodBag int, Donor_Id int not null, Retr_Date Date not null, 
 primary key (id_BloodBag), foreign key (Donor_Id) references DONOR(id_Donor));
 
-create table INVENTORY(id_BloodBag int, id_Bank int, primary key (id_BloodBag, id_Bank), 
-foreign key (id_Bloodbag) references BLOODBAG(id_BloodBag), 
-foreign key (id_Bank) references BANK(id_Bank));
+create table INVENTORY(Bloodbag_id int, Bank_id int, primary key (Bloodbag_id, Bank_id), 
+foreign key (Bloodbag_id) references BLOODBAG(id_Bloodbag), 
+foreign key (Bank_id) references BANK(id_Bank));
 
-create table VACCOUNT(id_Account int, id_Vampire int not null, preference int, 
+create table VACCOUNT(id_Account int, Vampire_id int not null, preference int, 
 allowance int, Create_Date Date, primary key (id_Account), 
-foreign key (id_Vampire) references VAMPIRE(id_Vampire), 
+foreign key (Vampire_id) references VAMPIRE(id_Vampire), 
 foreign key (preference) references BLOODTYPE(id_Bloodtype));
 
-create table Transaction(id_Account int, id_BloodBag int, 
-TDate date not null, primary key(id_Account, id_BloodBag), 
-foreign key (id_Account) references VACCOUNT(id_Account), 
-foreign key (id_BloodBag) references BLOODBAG(id_BloodBag);
+create table Transaction(Account_id int, Bloodbag_id int, 
+TDate date not null, primary key(Account_id, Bloodbag_id), 
+foreign key (Account_id) references VACCOUNT(id_Account), 
+foreign key (Bloodbag_id) references BLOODBAG(id_BloodBag));
 
 insert into BLOODTYPE(id_Bloodtype, bType) values (1, 'A+');
 insert into BLOODTYPE(id_Bloodtype, bType) values (2, 'A-');
@@ -41,7 +41,7 @@ insert into BLOODTYPE(id_Bloodtype, bType) values (6, 'O-');
 insert into BLOODTYPE(id_Bloodtype, bType) values (7, 'AB+');
 insert into BLOODTYPE(id_Bloodtype, bType) values (8, 'AB-');
 
-insert into DONOR(id_Donor, id_BloodType, Lst_Donation, Pers_Info, Date_Start) 
+insert into DONOR(id_Donor, Bloodtype_id, Lst_Donation, Pers_Info, Date_Start) 
 values (101, 5, '2022-10-31', 'Likes Chocolate', '2020-1-1');
 
 insert into BLOODBAG(id_BLoodBag, Donor_Id, Retr_Date) values (1, 101, '2022-10-1');
@@ -49,10 +49,10 @@ insert into BLOODBAG(id_BloodBag, Donor_Id, Retr_Date) values(2, 101, '2022-10-3
 
 insert into Bank(id_Bank, Location, Open_Date) values (1, '1012 Spooky Drive, Howl Michigan', '1856-10-31');
 
-insert into INVENTORY(id_BloodBag, id_Bank) values (2, 1);
+insert into INVENTORY(Bloodbag_id, Bank_id) values (2, 1);
 
 insert into VAMPIRE(id_Vampire, FName, LName) values(101, 'Michael', 'Morbius');
 
-insert into VACCOUNT(id_Account, id_Vampire, allowance, Create_Date) values (1, 101, 3, '2022-4-1');
+insert into VACCOUNT(id_Account, Vampire_id, allowance, Create_Date) values (1, 101, 3, '2022-4-1');
 
-insert into TRANSACTION(id_Account, id_BloodBag, TDate) values (1, 1, '2022-10-30');
+insert into TRANSACTION(Account_id, Bloodbag_id, TDate) values (1, 1, '2022-10-30');
